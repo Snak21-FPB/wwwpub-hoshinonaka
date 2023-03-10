15633eab6842adf56e629798da5e808a0eb3772f
Snak <72hoshino.snak@gmail.com>
2023-03-11 01:15:26 +0900

    fix style

--- pubsystem/pubsystem/tests.py
+++ pubsystem/pubsystem/tests.py
@@ -1,7 +1,9 @@
 from django.conf import settings
-from django.test import TestCase
-from accounts.models import Account
 from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from accounts.models import Account
+
 
 class TestDjangoSettings(TestCase):
     def test_language_setting(self):
@@ -9,6 +11,6 @@ class TestDjangoSettings(TestCase):
 
     def test_timezone_setting(self):
         self.assertEqual(settings.TIME_ZONE, "Asia/Tokyo")
-    
+
     def test_auth_model_setting(self):
         self.assertEqual(get_user_model(), Account)
