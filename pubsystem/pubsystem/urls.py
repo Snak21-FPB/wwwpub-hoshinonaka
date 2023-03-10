2739800e553a18e5b43a0d6e8e9088a2dfe2b095
Snak <72hoshino.snak@gmail.com>
2023-03-11 01:09:30 +0900

    add admin

--- pubsystem/pubsystem/urls.py
+++ pubsystem/pubsystem/urls.py
@@ -17,10 +17,9 @@ Including another URLconf
 """
 
 # Uncomment next two lines to enable admin:
-# from django.contrib import admin
-# from django.urls import path
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    # Uncomment the next line to enable the admin:
-    # path('admin/', admin.site.urls) # noqa
+    path('admin/', admin.site.urls),
 ]
