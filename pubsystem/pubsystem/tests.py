be701d7a388224758788b6fd45504a8b029a312c
Snak <72hoshino.snak@gmail.com>
2023-03-11 00:56:11 +0900

    add test

--- pubsystem/pubsystem/tests.py
+++ pubsystem/pubsystem/tests.py
@@ -1,10 +1,14 @@
 from django.conf import settings
 from django.test import TestCase
+from accounts.models import Account
 
 
 class TestDjangoSettings(TestCase):
-    def test_django_language(self):
+    def test_language_setting(self):
         self.assertEqual(settings.LANGUAGE_CODE, "ja")
 
-    def test_django_timezone(self):
+    def test_timezone_setting(self):
         self.assertEqual(settings.TIME_ZONE, "Asia/Tokyo")
+    
+    def test_auth_model_setting(self):
+        self.assertIsInstance(settings.AUTH_USER_MODEL, Account)
