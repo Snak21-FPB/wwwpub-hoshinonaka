21ab65aaa04d74464d7bc74d7df68d17bd7cfd30
Snak <72hoshino.snak@gmail.com>
2023-03-11 23:44:28 +0900

    add test comment

--- pubsystem/accounts/tests/test_models.py
+++ pubsystem/accounts/tests/test_models.py
@@ -17,6 +17,7 @@ class TestAuthUserModel(TestCase):
         self.user = get_user_model().objects.get(username="user1")
 
     def test_date_joined(self):
+        """登録日時のテスト"""
         self.assertEqual(
             self.user.date_joined,
             datetime.datetime(2023, 2, 1, 12, 12, 12, tzinfo=datetime.timezone.utc),
@@ -24,6 +25,7 @@ class TestAuthUserModel(TestCase):
 
     @freezegun.freeze_time("2023-02-01 12:12:21")
     def test_last_login(self):
+        """最終ログイン日時のテスト"""
         self.client.force_login(self.user)
         self.assertEqual(
             self.user.last_login,
--- pubsystem/pubsystem/tests.py
+++ pubsystem/pubsystem/tests.py
@@ -7,10 +7,13 @@ from accounts.models import Account
 
 class TestDjangoSettings(TestCase):
     def test_language_setting(self):
+        """言語設定"""
         self.assertEqual(settings.LANGUAGE_CODE, "ja")
 
     def test_timezone_setting(self):
+        """タイムゾーン設定"""
         self.assertEqual(settings.TIME_ZONE, "Asia/Tokyo")
 
     def test_auth_model_setting(self):
+        """認証ユーザーモデル設定"""
         self.assertEqual(get_user_model(), Account)
