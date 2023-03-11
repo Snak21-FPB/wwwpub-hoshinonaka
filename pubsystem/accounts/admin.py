3fac265107a393e0a6703afd9a0bae98cf9140b4
Snak <72hoshino.snak@gmail.com>
2023-03-11 13:35:39 +0900

    fix models

--- pubsystem/accounts/admin.py
+++ pubsystem/accounts/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from . import models
+from .models import Account
 
-# Register your models here.
-admin.site.register(models.Account)
+class AccountAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields":("email","username","password")}),
+        (("権限"),{"fields":("is_active","is_staff")}),
+        (("ログイン記録"),{"fields":("date_joined",)})
+    )
+
+admin.site.register(Account, AccountAdmin)
--- pubsystem/accounts/models.py
+++ pubsystem/accounts/models.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 # Create your models here.
 
 
 class Account(AbstractUser):
-    pass
+    email = models.EmailField(verbose_name="メールアドレス", unique=True)
+
+    REQUIRED_FIELDS = ("username",)
+    USERNAME_FIELD = "email"
