b24248e7a4722f3d88e3540aadc213ba28a83dcd
Snak <72hoshino.snak@gmail.com>
2023-03-11 23:37:01 +0900

    fix style

--- pubsystem/accounts/tests/test_models.py
+++ pubsystem/accounts/tests/test_models.py
@@ -1,21 +1,31 @@
-from django.test import TestCase
-import freezegun
 import datetime
+
+import freezegun
 from django.contrib.auth import get_user_model
+from django.test import TestCase
+
 
 class TestAuthUserModel(TestCase):
     @classmethod
-    @freezegun.freeze_time("2023-02-01 12:34:56")
+    @freezegun.freeze_time("2023-02-01 12:12:12")
     def setUpTestData(cls):
-        get_user_model().objects.create(username="user1", email="user1@example.com", password="user1spassword")
-    
+        get_user_model().objects.create(
+            username="user1", email="user1@example.com", password="user1spassword"
+        )
+
     def setUp(self):
         self.user = get_user_model().objects.get(username="user1")
-        
+
     def test_date_joined(self):
-        self.assertEqual(self.user.date_joined, datetime.datetime(2023,2,1,12,34,56,tzinfo=datetime.timezone.utc))
-    
+        self.assertEqual(
+            self.user.date_joined,
+            datetime.datetime(2023, 2, 1, 12, 12, 12, tzinfo=datetime.timezone.utc),
+        )
+
+    @freezegun.freeze_time("2023-02-01 12:12:21")
     def test_last_login(self):
-        # logged_in = self.client.login(email="user1@example.com", password="user1spassword")
-        # self.assertTrue(logged_in)
-        self.assertEqual(self.user.last_login, datetime.datetime(2023,2,1,12,34,56,tzinfo=datetime.timezone.utc))
+        self.client.force_login(self.user)
+        self.assertEqual(
+            self.user.last_login,
+            datetime.datetime(2023, 2, 1, 12, 12, 21, tzinfo=datetime.timezone.utc),
+        )
