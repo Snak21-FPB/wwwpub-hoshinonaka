3ab1068c42d809933c3bca5c3c0ea5b77c6a3033
Snak <72hoshino.snak@gmail.com>
2023-03-11 15:38:35 +0900

    fix admin

--- pubsystem/accounts/admin.py
+++ pubsystem/accounts/admin.py
@@ -14,5 +14,10 @@ class AccountAdmin(UserAdmin):
         (None, {"classes":("wide",),"fields":("email","username","password1","password2")}),
     )
 
+    list_display = ("username", "email", "is_active", "is_staff", "last_login")
+    list_filter = ("is_active", "is_staff")
+    search_fields = ("username","email")
+    ordering = ("date_joined",)
+
 admin.site.register(Account, AccountAdmin)
 
