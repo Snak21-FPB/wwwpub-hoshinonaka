e038130a801b9f675f3571dd58b984796c915900
Snak <72hoshino.snak@gmail.com>
2023-03-12 23:36:20 +0900

    fix style

--- pubsystem/posts/models.py
+++ pubsystem/posts/models.py
@@ -1,16 +1,31 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from markdownx.models import MarkdownxField
-from django.contrib.auth import get_user_model
+
 
 class Bureau(models.Model):
     class Meta:
         db_table = "bureaus"
-    
+
     name = models.CharField(verbose_name="名称", max_length=16, unique=True)
-    keyword = models.SlugField(verbose_name="キーワード", unique=True, help_text="英数字と-が利用できます。URLに使用される文字列です。")
+    keyword = models.SlugField(
+        verbose_name="キーワード", unique=True, help_text="英数字と-が利用できます。URLに使用される文字列です。"
+    )
     outline = MarkdownxField("局概要", help_text="マークダウン形式で入力してください。")
-    sort_num = models.SmallIntegerField(verbose_name="ソート値",help_text="整数を入力してください。値が小さいほど上に表示されます。")
+    sort_num = models.SmallIntegerField(
+        verbose_name="ソート値", help_text="整数を入力してください。値が小さいほど上に表示されます。"
+    )
     created_at = models.DateTimeField(verbose_name="作成日時", auto_now_add=True)
-    created_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name="bureau_creator",null=True)
+    created_by = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.SET_NULL,
+        related_name="bureau_creator",
+        null=True,
+    )
     updated_at = models.DateTimeField(verbose_name="最終更新日時", auto_now=True)
-    updated_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name="bureau_updater",null=True)
+    updated_by = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.SET_NULL,
+        related_name="bureau_updater",
+        null=True,
+    )
--- pubsystem/posts/tests/test_models.py
+++ pubsystem/posts/tests/test_models.py
@@ -1,24 +1,36 @@
-from django.test import TestCase
-import freezegun
-from posts.models import Bureau
-from datetime import datetime
 import datetime as dt
+from datetime import datetime
+
+import freezegun
+from django.test import TestCase
+
+from posts.models import Bureau
+
 
 class TestBureauModel(TestCase):
     @classmethod
     @freezegun.freeze_time("2023-02-01 21:21:21")
     def setUpTestData(cls):
-        bureaus = [Bureau(name=f"テスト局{i}",keyword=f"test_bureau_{i}",sort_num=10-i) for i in range(10)]
+        bureaus = [
+            Bureau(name=f"テスト局{i}", keyword=f"test_bureau_{i}", sort_num=10 - i)
+            for i in range(10)
+        ]
         Bureau.objects.bulk_create(bureaus)
 
     def setUp(self):
         self.bureau = Bureau.objects.get(name="テスト局1")
 
     def test_created_at(self):
-        self.assertEqual(self.bureau.created_at, datetime(2023,2,1,21,21,21, tzinfo=dt.timezone.utc))
-    
+        self.assertEqual(
+            self.bureau.created_at,
+            datetime(2023, 2, 1, 21, 21, 21, tzinfo=dt.timezone.utc),
+        )
+
     @freezegun.freeze_time("2023-02-01 22:22:22")
     def test_updated_at(self):
         self.bureau.outline = "更新"
         self.bureau.save()
-        self.assertEqual(self.bureau.updated_at, datetime(2023,2,1,22,22,22,tzinfo=dt.timezone.utc))
+        self.assertEqual(
+            self.bureau.updated_at,
+            datetime(2023, 2, 1, 22, 22, 22, tzinfo=dt.timezone.utc),
+        )
