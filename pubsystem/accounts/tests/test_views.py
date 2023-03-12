7479343c67ea601be3da5225a7be72c860624206
Snak <72hoshino.snak@gmail.com>
2023-03-12 17:06:40 +0900

    remove login test

--- pubsystem/accounts/tests/test_views.py
+++ pubsystem/accounts/tests/test_views.py
@@ -18,21 +18,3 @@ class TestAccountLoginView(TestCase):
     def test_can_view(self):
         response = self.client.get(self.view)
         self.assertEqual(response.status_code, 200)
-
-    def test_valid_login(self):
-        response = self.client.post(
-            self.view, {"email": "testuser@example.com", "password": "test2023user"}
-        )
-        form = response.context["form"]
-        self.assertRedirects(response, reverse(settings.LOGIN_REDIRECT_URL))
-        self.assertTrue(form.is_valid())
-        self.assertIn(SESSION_KEY, self.client.session)
-
-    def test_invalid_login(self):
-        response = self.client.post(
-            self.view, {"email": "testuser@example.com", "password": "invalidpass"}
-        )
-        form = response.context["form"]
-        self.assertEqual(response.status_code, 200)
-        self.assertFalse(form.is_valid())
-        self.assertNotIn(SESSION_KEY, self.client.session)
