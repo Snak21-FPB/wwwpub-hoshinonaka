0645d02dde7d15b47205cc707fde314da712b1d4
Snak <72hoshino.snak@gmail.com>
2023-03-13 00:14:41 +0900

    fix admin

--- pubsystem/posts/admin.py
+++ pubsystem/posts/admin.py
@@ -6,4 +6,10 @@ from . import models
 class BureauAdmin(ModelAdmin):
     list_display = ("name", "created_by", "created_at", "updated_by", "updated_at")
 
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by:
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        return super().save_model(request, obj, form, change)
+
 admin.site.register(models.Bureau,BureauAdmin)
